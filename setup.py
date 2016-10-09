73fa45260d2d41dc6335900272cd231c4a55e76d
Max Wittig <max.wittig95@gmail.com>
2016-10-10 01:25:24 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='terminal_timekeeper',
+    version='1.0.0',
+    packages=[''],
+    data_files=['server_info.json'],
+    url='',
+    license='',
+    author='spaghettic0der',
+    author_email='',
+    description=''
+)
